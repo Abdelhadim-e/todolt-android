1ed5a6e7ab91fc0032334ff8e8d9514d36e8bf4b
Abdelhadim-e <e.abdelhadim@gmail.com>
2020-05-08 16:44:05 +0200

    add class taskDhelper for open bdd

--- app/src/main/java/com/example/todolist/db/TaskContract.java
+++ app/src/main/java/com/example/todolist/db/TaskContract.java
@@ -1,4 +1,14 @@
 package com.example.todolist.db;
 
+import android.provider.BaseColumns;
+
 public class TaskContract {
+    //bd
+    public static final String DB_NAME = "com.example.todolist.db";
+    public static final int BD_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns{
+        public static final String TABLE = "task";
+        public static final String COL_TASK_TITLE = "title";
+    }
 }
